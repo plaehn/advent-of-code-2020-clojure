b01a0ee20a68b237b53595119d3337adb0d417f0
Oliver Plaehn <oliver.plaehn@gmail.com>
2020-12-31 21:10:54 +0100

    Refactor for solution for part 2

--- src/advent/of/code/2020/day2.clj
+++ src/advent/of/code/2020/day2.clj
@@ -2,11 +2,11 @@
   (:require [clojure.string :as str]))
 
 (defn parse [line]
-  (let [[min-str max-str chr-str pwd] (str/split line #"[-: ]+")
-        chr (first chr-str)
-        min (read-string min-str)
-        max (read-string max-str)]
-    (list min max chr pwd)))
+  (let [[num1-str num2-str chr-str pwd] (str/split line #"[-: ]+")
+        num1 (read-string num1-str)
+        num2 (read-string num2-str)
+        chr (first chr-str)]
+    (list num1 num2 chr pwd)))
 
 (def lines
   (map #(parse %) (str/split-lines (slurp "day2.txt"))))
@@ -14,10 +14,10 @@
 (defn count-char [chr str]
   (get (frequencies str) chr 0))
 
-(defn match? [line]
+(defn match-for-first-policy? [line]
   (let [[min max chr pwd] line]
     (<= min (count-char chr pwd) max)))
 
-(def result-part-1 (count (filter match? lines)))
+(def result-part-1 (count (filter match-for-first-policy? lines)))
 (println (str "Solution part 1: " result-part-1))
 (assert (= result-part-1 460))
