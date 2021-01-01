7d33d98e61af0551245dd52240c7cadafe507289
Oliver Plaehn <oliver.plaehn@gmail.com>
2021-01-01 13:04:36 +0100

    Solution for day 4, part 2

--- src/advent/of/code/2020/day4.clj
+++ src/advent/of/code/2020/day4.clj
@@ -11,13 +11,40 @@
   (map #(parse-passport %) (str/split (slurp "day4.txt") #"\r?\n\r?\n")))
 
 
-(def required-attrs '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"))
+(def field-validators {"byr" #(<= 1920 (read-string %) 2002)
+                       "iyr" #(<= 2010 (read-string %) 2020)
+                       "eyr" #(<= 2020 (read-string %) 2030)
+                       "hgt" #(let [unit-start (- (count %) 2)
+                                    unit (subs % unit-start)
+                                    size-str (subs % 0 unit-start)
+                                    size (if (empty? size-str)
+                                           0
+                                           (read-string size-str))]
+                                (or
+                                  (and (= unit "cm") (<= 150 size 193))
+                                  (and (= unit "in") (<= 59 size 76))))
+                       "hcl" #(re-matches #"\#[0-9a-f]{6}" %)
+                       "ecl" #(contains? #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} %)
+                       "pid" #(re-matches #"[0-9]{9}" %)})
 
-(defn is-valid? [passport]
-  (every? #(contains? passport %) required-attrs))
+(defn contains-required-keys? [passport]
+  (every? #(contains? passport %) (keys field-validators)))
 
-(def valid-passport-count
-  (count (filter #(is-valid? %) passports)))
+(defn valid? [attr value]
+  ((field-validators attr) value))
 
-(println (str "Solution part 1: " valid-passport-count))
-(assert (= valid-passport-count 219))
+(defn values-are-valid? [passport]
+  (every? #(valid? % (passport %)) (keys field-validators)))
+
+(defn contains-required-keys-and-values-are-valid? [passport]
+  (and (contains-required-keys? passport) (values-are-valid? passport)))
+
+(def valid-passport-count-1
+  (count (filter #(contains-required-keys? %) passports)))
+(println (str "Solution part 1: " valid-passport-count-1))
+(assert (= valid-passport-count-1 219))
+
+(def valid-passport-count-2
+  (count (filter #(contains-required-keys-and-values-are-valid? %) passports)))
+(println (str "Solution part 2: " valid-passport-count-2))
+(assert (= valid-passport-count-2 127))
