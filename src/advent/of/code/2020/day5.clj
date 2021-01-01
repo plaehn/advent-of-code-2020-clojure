583b0bb61d3ae00920864d1ad22d7ce84623e706
Oliver Plaehn <oliver.plaehn@gmail.com>
2021-01-01 16:29:18 +0100

    Solution for day 5, part 1

---
+++ src/advent/of/code/2020/day5.clj
@@ -0,0 +1,32 @@
+(ns advent.of.code.2020.day5
+  (:require [clojure.string :as str]))
+
+(def boarding-passes
+  (str/split-lines (slurp "day5.txt")))
+
+(defn reduce-range [input max-val first-half-chr]
+  (first
+    (reduce
+      (fn [range chr]
+        (let [half-size (/ (count range) 2)]
+          (if (= chr first-half-chr)
+            (take half-size range)
+            (drop half-size range))))
+      (range 0 (inc max-val))
+      input)))
+
+(defn compute-seat-id [input]
+  (let [divider (- (count input) 3)
+        row-str (subs input 0 divider)
+        col-str (subs input divider)
+        row (reduce-range row-str 127 \F)
+        col (reduce-range col-str 7 \L)
+        seat-id (+ (* 8 row) col)]
+    seat-id))
+
+(def max-seat-id
+  (apply max (map #(compute-seat-id %) boarding-passes)))
+
+(println (str "Solution part 1: " max-seat-id))
+(assert (= max-seat-id 826))
+
